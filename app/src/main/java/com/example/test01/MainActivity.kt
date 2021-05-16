dd4037dff7d942e80bf95c01e5f1ef1bdfba623a
dennisraabe <dennis.raabe@basix.de>
2021-05-16 20:34:06 +0200

    Code erweitert um Variablen, Constraints gesetzt.

--- app/src/main/java/com/example/test01/MainActivity.kt
+++ app/src/main/java/com/example/test01/MainActivity.kt
@@ -2,12 +2,26 @@ package com.example.test01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var editTextLiter = findViewById<EditText>(R.id.mengeLiter)
+        var editTextVol = findViewById<>(R.id.alkoholgehaltVol_)
+        var editTextGewicht = findViewById<>(R.id.koerperGewicht)
+        var radioButtonWeiblich = findViewById<>(R.id.geschlechtFrau)
+        var radioButton = findViewById<>(R.id.geschlechtMann)
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener{
+            Toast.makeText(this, editTextLiter.text, Toast.LENGTH_SHORT).show()
+
     }
-    operator fun div() Te
+
+
+    }
 }
