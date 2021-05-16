fe8915d66fc6589902bd70c2a5d2965035c76723
dennisraabe <dennis.raabe@basix.de>
2021-05-16 21:08:26 +0200

    Variablen fertig.

--- app/src/main/java/com/example/test01/MainActivity.kt
+++ app/src/main/java/com/example/test01/MainActivity.kt
@@ -2,10 +2,7 @@ package com.example.test01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.RadioButton
-import android.widget.Toast
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,6 +14,11 @@ class MainActivity : AppCompatActivity() {
         var editTextGewicht = findViewById<EditText>(R.id.koerperGewicht)
         var radioButtonWeiblich = findViewById<RadioButton>(R.id.geschlechtFrau)
         var radioButton = findViewById<RadioButton>(R.id.geschlechtMann)
+        var textViewMenge = findViewById<TextView>(R.id.alkoholMenge)
+        var textViewAktuell = findViewById<TextView>(R.id.aktuellPromille)
+        var textViewStunden05 = findViewById<TextView>(R.id.stunden05)
+        var textViewStunden00 = findViewById<TextView>(R.id.stunden00)
+
         val button = findViewById<Button>(R.id.button)
         button.setOnClickListener{
             Toast.makeText(this, editTextLiter.text, Toast.LENGTH_SHORT).show()
