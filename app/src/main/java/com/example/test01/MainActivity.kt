816b809428367b7327e772cd8b117144ef44eab3
dennisraabe <dennis.raabe@basix.de>
2021-05-18 11:26:43 +0200

    Rechnung hinzugefügt. Variablen für rechnung ergänzt

--- app/src/main/java/com/example/test01/MainActivity.kt
+++ app/src/main/java/com/example/test01/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.test01
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
 import android.widget.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         var editTextLiter = findViewById<EditText>(R.id.mengeLiter)
-        var editTextVol = findViewById<EditText>(R.id.alkoholgehaltVol_)
+        var editTextVol = findViewById<EditText>(R.id.alkoholgehaltVol)
         var editTextGewicht = findViewById<EditText>(R.id.koerperGewicht)
         var radioButtonWeiblich = findViewById<RadioButton>(R.id.geschlechtFrau)
         var radioButton = findViewById<RadioButton>(R.id.geschlechtMann)
@@ -19,12 +20,35 @@ class MainActivity : AppCompatActivity() {
         var textViewStunden05 = findViewById<TextView>(R.id.stunden05)
         var textViewStunden00 = findViewById<TextView>(R.id.stunden00)
 
+        var result: Double
+
+
+
+
+
         val button = findViewById<Button>(R.id.button)
         button.setOnClickListener{
-            Toast.makeText(this, editTextLiter.text, Toast.LENGTH_SHORT).show()
 
-    }
+            var numberLiter = editTextLiter.text.toString().toDouble()
+            var numberVol = editTextVol.text.toString().toDouble()
+            var numberGewicht = editTextGewicht.text.toString().toDouble()
+
+            result = numberLiter / 100 * numberVol
+            textViewMenge.text = result.toString()
+
+
+        }
+       // operator fun Editable.div(i: Int): Double {
+
+       // mengeLiter : 100 * alkoholgehaltVol = alkoholMenge
+
+       // alkoholmenge : koerperGewicht * geschlecht =   aktuellPromille
+
+        //aktuellPromille : stündlicherPromilleAbbau = stundenBis0
 
 
     }
 }
+
+
+
